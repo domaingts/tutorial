3b701767652c0b9b5af3edafdb93d0731dd2421f
domaingts <139693268+domaingts@users.noreply.github.com>
2025-06-14 03:25:40 +0000

    fix a bug

--- common/tls/reality_server.go
+++ common/tls/reality_server.go
@@ -29,10 +29,6 @@ type RealityServerConfig struct {
 
 func NewRealityServer(ctx context.Context, logger log.Logger, options option.InboundTLSOptions) (*RealityServerConfig, error) {
 	var tlsConfig reality.Config
-
-	if options.ACME != nil && len(options.ACME.Domain) > 0 {
-		return nil, E.New("acme is unavailable in reality")
-	}
 	tlsConfig.Time = ntp.TimeFuncFromContext(ctx)
 	if options.ServerName != "" {
 		tlsConfig.ServerName = options.ServerName
