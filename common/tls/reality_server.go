4c92a3165d3ea8f63433b948225ad031777b1cd8
domaingts <139693268+domaingts@users.noreply.github.com>
2025-06-14 03:17:50 +0000

    try to fix some problems

--- common/tls/reality_server.go
+++ common/tls/reality_server.go
@@ -1,3 +1,5 @@
+//go:build with_reality_server
+
 package tls
 
 import (
@@ -27,6 +29,10 @@ type RealityServerConfig struct {
 
 func NewRealityServer(ctx context.Context, logger log.Logger, options option.InboundTLSOptions) (*RealityServerConfig, error) {
 	var tlsConfig reality.Config
+
+	if options.ACME != nil && len(options.ACME.Domain) > 0 {
+		return nil, E.New("acme is unavailable in reality")
+	}
 	tlsConfig.Time = ntp.TimeFuncFromContext(ctx)
 	if options.ServerName != "" {
 		tlsConfig.ServerName = options.ServerName
