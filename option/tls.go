e9524f63836d8d91db4cd1baacd7fc076e01bc40
domaingts <139693268+domaingts@users.noreply.github.com>
2025-07-30 04:15:09 +0000

    fix some bugs

--- option/tls.go
+++ option/tls.go
@@ -70,7 +70,7 @@ func (o *OutboundTLSOptionsContainer) ReplaceOutboundTLSOptions(options *Outboun
 
 type InboundRealityOptions struct {
 	Enabled           bool                           `json:"enabled,omitempty"`
-	Show              bool                           `json:"enabled,omitzero"`
+	Show              bool                           `json:"show,omitzero"`
 	Handshake         InboundRealityHandshakeOptions `json:"handshake,omitzero"`
 	PrivateKey        string                         `json:"private_key,omitempty"`
 	ShortID           badoption.Listable[string]     `json:"short_id,omitempty"`
