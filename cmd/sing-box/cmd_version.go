69cce556fd59486c5720b0a682e804348089440e
domaingts <139693268+domaingts@users.noreply.github.com>
2026-01-02 03:27:43 +0000

    add build time

--- cmd/sing-box/cmd_version.go
+++ cmd/sing-box/cmd_version.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"runtime"
 	"runtime/debug"
+	"time"
 
 	C "github.com/sagernet/sing-box/constant"
 
@@ -19,7 +20,10 @@ var commandVersion = &cobra.Command{
 
 var nameOnly bool
 
+var buildTime string
+
 func init() {
+	buildTime = time.Now().In(time.FixedZone("fixed", 3600*8)).Format("2006.1.2")
 	commandVersion.Flags().BoolVarP(&nameOnly, "name", "n", false, "print version name only")
 	mainCommand.AddCommand(commandVersion)
 }
@@ -29,7 +33,7 @@ func printVersion(cmd *cobra.Command, args []string) {
 		os.Stdout.WriteString(C.Version + "\n")
 		return
 	}
-	version := "sing-box version " + C.Version + "\n\n"
+	version := "sing-box version " + C.Version + " " + buildTime + "\n\n"
 	version += "Environment: " + runtime.Version() + " " + runtime.GOOS + "/" + runtime.GOARCH + "\n"
 
 	var tags string
